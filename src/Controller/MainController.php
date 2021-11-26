0cff77d197e88ee19b9b037f7c1c0b0824988a28
Martin Polak <Martin Polak@itb.int>
2021-11-26 23:56:48 +0100

    first controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,4 +16,13 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    #[Route('/start/{name?}', name: 'start')]
+    public function start(Request $request, $name)
+    {
+        return new Response('<h1>Willkommen '.$name.'</h1>');
+    }
+
+
+
 }
